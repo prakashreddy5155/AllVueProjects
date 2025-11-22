98cd3e61726dc526c3f0cae51f9080ee2aeb7338
prakashreddy5155 <prakashreddy5155@gmail.com>
2025-11-22 11:45:35 +0530

    feat: basicCounter

---
+++ basic-statemanagement-project-revise-fourth-time/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+
+
+
+  <div class="displayCont">
+    {{ this.$data.counter }}
+  </div>
+
+  <div class="buttonsCont">
+
+    <button @click="decreaseCounter">
+      -
+    </button>
+    <button @click="increaseCounter">
+      +
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data() 
+  {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter()
+    {
+      this.$data.counter++;
+    },
+    decreaseCounter()
+    {
+      this.$data.counter--;
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.displayCont 
+{
+  font-size:40px;
+}
+.buttonsCont button 
+{
+  height:50px;
+  width:50px;
+  margin:5px;
+}
+</style>
